2f067878be7ecd86d5f29dbedab84d28b9040014
Arınç Demir <68223368+arincdemir@users.noreply.github.com>
2021-12-16 03:11:07 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import keyboard
+
+keyboard.add_hotkey('alt+shift+z', lambda: keyboard.write("çerEZ mayonEZ ançüEZ çerkEZ körfEZ merkEZ hipotEZ muazzEZ polonEZ çekelEZ pekmEZ protEZ sentEZ trapEZ manganEZ parantEZ varyemEZ filogenEZ güngörmEZ fotosentEZ hıdırellEZ karyokinEZ sugötürmEZ EZEZEZEZEZEZEZEZ"))
+
+keyboard.wait()
